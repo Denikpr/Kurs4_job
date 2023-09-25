78b8fc99d854e0c730a438203f3d86a177837056
Denis Prusakov <Denikpr@gmail.com>
2023-09-25 15:31:44 +0300

    userinput

--- src/class_api.py
+++ src/class_api.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-
+import copy
 
 class API(ABC):
 
@@ -7,9 +7,55 @@ class API(ABC):
     def __init__(self):
         pass
 
+    @abstractmethod
+    def get_vacancies(self):
+        pass
+
+    @abstractmethod
+    def change_date(self):
+        pass
+
+    @abstractmethod
+    def add_word(self):
+        pass
+
+    @abstractmethod
+    def add_area(self):
+        pass
+
+    @abstractmethod
+    def load_all_areas(self):
+        pass
+
 
 class HeadHunter_API(API):
-    pass
+
+    HH_API_URL = 'https://api.hh.ru/vacancies'
+    HH_API_URL_AREAS = 'https://api.hh.ru/areas'
+
+    param_zero ={
+        'per_page': 100
+    }
+
+    def __init__(self):
+        self.param = copy.deepcopy(self.param_zero)
+        pass
+
+    def get_vacancies(self):
+        pass
+
+    def change_date(self):
+        pass
+
+    def add_word(self):
+        pass
+
+    def add_area(self):
+        pass
+
+    def load_all_areas(self):
+        pass
+
 
 class SuperJob_API(API):
     pass
