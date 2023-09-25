34bd5e332498cda28521ebb70e4686fa614ef9ff
Denis Prusakov <Denikpr@gmail.com>
2023-09-25 14:27:41 +0300

    userinput

--- src/class_userinput.py
+++ src/class_userinput.py
@@ -1,12 +1,14 @@
 from src.class_api import HeadHunter_API, SuperJob_API
 from src.class_mylist import MyList
+import copy
 
 class UserInput:
     pass
-
+    param_zero = {}
     def __init__(self):
         self.hh_api = HeadHunter_API()
         self.sj_api = SuperJob_API()
-        self.my_list = MyList()
-    def __call__(self):
+        self.all_list = MyList()
+        self.favorite_list = MyList()
+        selg.param
         pass
