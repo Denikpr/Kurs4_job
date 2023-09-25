30de93f5bc0355ed286f3b148947fba7c180a1c7
Denis Prusakov <Denikpr@gmail.com>
2023-09-25 17:01:38 +0300

    class_api

--- src/class_api.py
+++ src/class_api.py
@@ -60,11 +60,40 @@ class HeadHunter_API(API):
         pass
 
     def load_all_areas(self):
-        pass
-
+        response = requests.get(self.HH_API_URL_AREAS)
+        return response.json()
 
 class SuperJob_API(API):
-    pass
+
+    SJ_API_URL = 'https://api.superjob.ru/2.0/vacancies/'
+    SJ_API_URL_AREAS = 'https://api.superjob.ru/2.0/towns/'
+
+    param_zero ={
+        'text': 'python',
+        'per_page': 100,
+        'area': 47,
+        'data': 14
+    }
+
+    def __init__(self):
+        self.param = copy.deepcopy(self.param_zero)
+        pass
+
+    def get_vacancies(self):
+        response = requests.get(self.HH_API_URL, self.param)
+        return response.json()["items"]
+    def change_date(self):
+        pass
+
+    def add_word(self):
+        pass
+
+    def add_area(self):
+        pass
+
+    def load_all_areas(self):
+        response = requests.get(self.HH_API_URL_AREAS)
+        return response.json()
 
 
 
